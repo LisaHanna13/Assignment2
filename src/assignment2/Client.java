9ab1e5584ab3d91bfaa54fce7c27bd5e39b451ca
LisaHanna13 <lisa-hanna@hotmail.com>
2020-05-02 01:04:40 -0400

    Final commit of the night; CLIENT FILE: Added scanners to read user input AND read files; created variables for different accounts

--- src/assignment2/Client.java
+++ src/assignment2/Client.java
@@ -2,11 +2,29 @@
  * This is the package for Assignment 2
  */
 package assignment2;
-
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 /**
  * This is the class that will be used to execute the constructors and methods
  * @author lisa-
  */
 public class Client {
+    /**
+     * This is the main method used for this class. 
+     * @param args the command line arguments
+     * @throws FileNotFoundException 
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        //Create a scanner that will read a file, and a scanner for user input
+        File inFile = new File("AccountNamesAndBalance.txt");
+        Scanner readFile = new Scanner(inFile);
+        Scanner keyboard = new Scanner(System.in);
+        
+        //Create variables for the different accounts
+        String accountName1, accountName2, accountName3;
+        double bankBalance1, bankBalance2, bankBalance3;
+        
+    }
     
 }
