bad6bc37a3558cc67e71c348afd7083017595ef7
LisaHanna13 <lisa-hanna@hotmail.com>
2020-05-02 15:13:41 -0400

    Reading the text file to be able to create the 3 account objects (with the help of the constructor)

--- src/assignment2/Client.java
+++ src/assignment2/Client.java
@@ -24,6 +24,23 @@ public class Client {
         //Create variables for the different accounts
         String accountName1, accountName2, accountName3;
         double bankBalance1, bankBalance2, bankBalance3;
+        //Create variables to skip over empty lines, so there is no issue reading files
+        String blank1, blank2;
+        
+        //Creating objects
+        accountName1 = readFile.nextLine();
+        bankBalance1 = readFile.nextDouble();
+        Account account1 = new Account(accountName1, bankBalance1, "4833-1200-3412-3456-A");
+        
+        blank1 = readFile.nextLine();
+        accountName2 = readFile.nextLine();
+        bankBalance2 = readFile.nextDouble();
+        Account account2 = new Account(accountName2, bankBalance2, "4217-6453-2453-3145-B");
+        
+        blank2 = readFile.nextLine();
+        accountName3 = readFile.nextLine();
+        bankBalance3 = readFile.nextDouble();
+        Account account = new Account(accountName3, bankBalance3, "5353-6572-6475-3646-C");
         
     }
     
