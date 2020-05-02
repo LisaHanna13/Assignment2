007699e05733c711632a4ea790f849a891aa509d
LisaHanna13 <lisa-hanna@hotmail.com>
2020-05-01 20:39:36 -0400

    Adding the Account class to the repository

---
+++ src/assignment2/Account.java
@@ -0,0 +1,12 @@
+/*
+ * This is the package for Assignment 2
+ */
+package assignment2;
+
+/**
+ * This class will be used for creating constructors and methods.
+ * @author lisa-
+ */
+public class Account {
+
+}
