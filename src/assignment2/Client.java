f43feadecd0f08187fdfe1ddb5d0e8a8eebd70ce
LisaHanna13 <lisa-hanna@hotmail.com>
2020-05-01 20:39:52 -0400

    Adding the Client class to the repository

---
+++ src/assignment2/Client.java
@@ -0,0 +1,12 @@
+/*
+ * This is the package for Assignment 2
+ */
+package assignment2;
+
+/**
+ * This is the class that will be used to execute the constructors and methods
+ * @author lisa-
+ */
+public class Client {
+    
+}
