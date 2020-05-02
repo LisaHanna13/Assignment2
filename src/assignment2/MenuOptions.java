68aa450f1cd6d4ae3c85a70a8ef129a8693b7ed4
LisaHanna13 <lisa-hanna@hotmail.com>
2020-05-02 15:52:48 -0400

    Create a new class that lets the programmer print a menu option without having to write the entire thing in the Client class (main class). Note: I do know this could be done directly in Client class, I just find it cleaner and clearer this way.

---
+++ src/assignment2/MenuOptions.java
@@ -0,0 +1,27 @@
+/*
+ * This is the package for assignment 2.
+ */
+package assignment2;
+
+/**
+ * This class will be used to print the menu options
+ * @author lisa-
+ */
+public class MenuOptions {
+    
+    /**
+     * This empty constructor can be used to create a menu object
+     */
+    public MenuOptions(){}
+    
+    public String toString(){
+        return "Please choose one option: " +
+               "\n----------------------------------------" +
+               "\n1- Print Bank Balance" +
+               "\n2- Deposit Money Into Account" +
+               "\n3- Withdraw Money From Account" +
+               "\n4- Transfer Money to Another Account" +
+               "\n----------------------------------------";
+    }
+    
+}
