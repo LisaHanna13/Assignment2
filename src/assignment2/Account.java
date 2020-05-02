d2a03820296d662130d8d89a6ebbfb08a091d80f
LisaHanna13 <lisa-hanna@hotmail.com>
2020-05-02 14:48:08 -0400

    Account File: Creating 6 methods and updating the constructor as well as some variables.

--- src/assignment2/Account.java
+++ src/assignment2/Account.java
@@ -9,15 +9,86 @@ package assignment2;
  */
 public class Account {
     //Creating basic attributes
-    String acountName;
-    Double bankBalance, depositAmount, withdrawAmount;
+    String accountName = "", bankID = "";
+    double bankBalance, depositAmount, withdrawAmount;
+    //Creating static instance variables
     static int oneDollarBill = 1, fiveDollarBill = 5, tenDollarBill = 10, twentyDollarBill = 20,
                fiftyDollarBill = 50, hundredDollarBill = 100;
+    static double interestRate = 0.0009;
     
-    public Account(String accountName, double bankBalance){
-        this.acountName = accountName;
+    /**
+     * This constructor is used to set up basic information about a bank account
+     * @param accountName
+     * @param bankBalance 
+     * @param bankID
+     */
+    public Account(String accountName, double bankBalance, String bankID){
+        this.accountName = accountName;
         this.bankBalance = bankBalance;
     }
     
+    /**
+     * When the client withdraws an amount, the bankBalance must be set to a new value
+     * This is therefore a mutator (setter)
+     * @param BankBalance 
+     */
+    void withdraw(double bankBalance){
+        this.bankBalance = bankBalance;
+    }
+    
+    /**
+     * When the client deposits an amount, the bankBalance must be set to a new value
+     * This is therefore a mutator (setter)
+     * @param bankBalance 
+     */
+    void deposit(double bankBalance){
+        this.bankBalance = bankBalance;
+    }
+    
+    /**
+     * This accessor (getter) can be used to get the bank balance of an account when
+     * wanting to transfer money between accounts
+     * @return bankBalance
+     */
+    public double getBankBalance(){
+        return bankBalance;
+    }
+    
+    /**
+     * This accessor (getter) can be used to get the accountNames available when wanting to
+     * do a transfer between accounts
+     * @return accountName
+     */
+    public String getAccountName(){
+        return accountName;
+    }
+    
+    /**
+     * This equals method can be used for a client to see if he has a second bank account;
+     * if they do, there will be two matching bankID's
+     * @param other
+     * @return true if both bankID's are the same
+     */
+    public Boolean equals(Account other){
+        return (bankID.equals(other.bankID));
+    }
+    
+    /**
+     * Use this method for a clean statement with the account name, bank balance, bank ID, and interest rate
+     * @return a printed bank statement 
+     */
+    public String toString(){
+        return "---------------------------------------------" +
+               "\n               BANK STATEMENT" +
+               "\n---------------------------------------------" +
+               "\nAccount Name : " + this.accountName +
+               "\nBank Balance : " + this.bankBalance +
+               "\nBank ID      : " + this.bankID +
+               "\nInterest Rate: " + this.bankID +
+               "\n---------------------------------------------\n";
+    }
+
+    
+
 
 }
